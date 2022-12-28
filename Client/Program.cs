78e5592c8962c5210b98f60244f2439296eb815e
Daitarus <chajkovskij_99@mail.ru>
2022-12-28 16:59:07 +0300

    c4: add getting list all files

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,28 +8,9 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            PipeProtocolTransport.Client client = new PipeProtocolTransport.Client("MyPipe", ".", 10000);
-            byte[] fileBytes = ReadFile("test.txt");
-            client.Start();
-            Command com = new FileRequest(0, "test.txt", fileBytes);
-            client.SendCommand(com);
-            client.Close();
-        }
+            DirectoryInfo dir = new DirectoryInfo(@"C:\My_Job\");
+            FileInfo[] fileInfo = FileWorker.CheckSubFiles(dir, 2).ToArray();
 
-        static byte[] ReadFile(string fileName)
-        {
-            byte[] fileBytes = new byte[0];
-            FileInfo fileInfo = new FileInfo(fileName);
-            if (fileInfo.Exists)
-            {
-                fileBytes = new byte[fileInfo.Length];
-                using (FileStream fstream = System.IO.File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                {
-                    int read = fstream.Read(fileBytes);
-                }
-            }
-
-            return fileBytes;
-        }
+        }        
     }
 }
