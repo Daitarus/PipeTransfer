a5ce6143e4cecc345d2a423c189fcc673cfce61d
Daitarus <chajkovskij_99@mail.ru>
2023-01-24 11:10:57 +0300

    add ScanFileSystem to Client

--- Client/FileTransport.cs
+++ Client/FileTransport.cs
@@ -1,15 +1,11 @@
 ﻿using CommandsKit;
 using PipeProtocolTransport;
 using System.Text;
-using ConsoleWorker;
-using System.Timers;
 
 namespace Client
 {
-    internal static class FileWorker
+    internal static class FileTransport
     {
-        private static DateTime time = DateTime.MinValue;
-
         private static byte[] ReadFileBlock(FileStream fileStream, long start, int lengthBlock, long lengthFile)
         {
             if (lengthFile < start + lengthBlock)
@@ -32,13 +28,6 @@ namespace Client
                 int maxLengthBlock = FileRequest.maxSizeInfoAndData - fileInfoBytes.Length;
                 int amountBlocks = (int)Math.Ceiling((double)fileInfo.Length / (double)maxLengthBlock);
 
-                //test timer
-                var timer = new System.Timers.Timer(1000);
-                timer.AutoReset = true;
-                timer.Enabled = true;
-                DateTime start = DateTime.Now;
-                //
-
                 using (FileStream fileStream = File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
                     for (int i = 0; i < amountBlocks; i++)
@@ -46,24 +35,10 @@ namespace Client
                         byte[] fileBlock = ReadFileBlock(fileStream, i * maxLengthBlock, maxLengthBlock, fileInfo.Length);
                         Command com = new FileRequest(i, amountBlocks, fileInfoBytes, fileBlock);
                         if (!client.SendCommand(com))
-                            break;
-
-                        //timer++
-                        timer.Elapsed += OnTimedEvent;
-                        //
-
-                        string outStr = CreatorOutString.GetLoadString(fileInfo.Name, i, amountBlocks);
-                        Console.Write(outStr);                       
+                            break;            
                     }
-                    Console.WriteLine();
-                    Console.WriteLine("Time spend: {0}", time - start);
                 }
             }
-        }
-
-        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            time = e.SignalTime;
         }
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,32 +1,36 @@
 ﻿using CommandsKit;
 using PipeProtocolTransport;
 using System.IO;
+using ScanFileSystem;
 
 namespace Client
 {
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            Console.Write("Enter server name: ");
-            string serverName = Console.ReadLine();
+        {
+            FileInfo[] files = ScanFiles.GetFiles(new DirectoryInfo("C:\\Users\\User"), 2).ToArray();
+            DirectoryInfo[] dirs = ScanFiles.GetDirectories(new DirectoryInfo("C:\\Users\\User"), 2).ToArray();
 
-            PptClient client = new PptClient("MyPipe", serverName, 10000);
+            //Console.Write("Enter server name: ");
+            //string serverName = Console.ReadLine();
 
-            if (client.Start())
-            {
-                Console.WriteLine("Connect!");
-                Console.Write("Enter file name: ");
-                string fileName = Console.ReadLine();
-                FileInfo fileInfo = new FileInfo(fileName);
+            //PptClient client = new PptClient("MyPipe", serverName, 10000);
 
-                FileWorker.SendFile(fileInfo, client);
-            }
+            //if (client.Start())
+            //{
+            //    Console.WriteLine("Connect!");
+            //    Console.Write("Enter file name: ");
+            //    string fileName = Console.ReadLine();
+            //    FileInfo fileInfo = new FileInfo(fileName);
 
-            client.Close();
+            //    FileTransport.SendFile(fileInfo, client);
+            //}
 
-            Console.WriteLine("End!");
-            Console.ReadKey();
+            //client.Close();
+
+            //Console.WriteLine("End!");
+            //Console.ReadKey();
         }        
     }
 }
